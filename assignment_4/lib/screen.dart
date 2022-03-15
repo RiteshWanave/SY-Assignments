fa920976fea2ca4d5d64e4c5611627b326d81897
RiteshWanave <riteshwanave@gmail.com>
2022-03-15 20:25:02 +0530

    Assignment 4 in Flutter

--- assignment_4/lib/screen.dart
+++ assignment_4/lib/screen.dart
@@ -25,12 +25,13 @@ class _ScrollScreenState extends State<ScrollScreen> {
             SizedBox(
               height: 20.0,
             ),
-            InfoCard(title: 'C++',info: 'This is C++', pict: 'https://imgs.search.brave.com/HGRZdQGQ-oUks0TmXDo_9Nb3TXpR3tR1I-x0xTRk6Ag/rs:fit:918:1032:1/g:ce/aHR0cHM6Ly9yYXcu/Z2l0aHVidXNlcmNv/bnRlbnQuY29tL2lz/b2NwcC9sb2dvcy9t/YXN0ZXIvY3BwX2xv/Z28ucG5n',),
+            InfoCard(title: 'C++',info: 'This is C++', pict: 'https://brandslogos.com/wp-content/uploads/images/large/c-logo.png',),
             InfoCard(title: 'Java', info: 'This is Java', pict: 'https://1000logos.net/wp-content/uploads/2020/09/Java-Logo-500x313.png',),
             InfoCard(title: 'Python', info: 'This is Python', pict: 'https://1000logos.net/wp-content/uploads/2020/08/Python-Logo-500x313.png',),
             InfoCard(title: 'C', info: 'This is C', pict: 'https://img.icons8.com/color/452/c-programming.png',),
             InfoCard(title: 'Dart', info: 'This is Dart', pict: 'https://seeklogo.com/images/D/dart-programming-language-logo-FF81164A13-seeklogo.com.png',),
-            InfoCard(title: 'JS', info: 'This is JS', pict: 'https://1000logos.net/wp-content/uploads/2020/09/JavaScript-Logo-500x313.png',)
+            InfoCard(title: 'JS', info: 'This is JS', pict: 'https://1000logos.net/wp-content/uploads/2020/09/JavaScript-Logo-500x313.png',),
+            InfoCard(title: 'Rubby', info: 'This is Rubby', pict: 'https://www.ruby-lang.org/images/header-ruby-logo.png',),
           ],
         ),
       ),
